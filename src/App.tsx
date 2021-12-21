261ce13c54d7af464e63f22d402183cf7593c6b6
Rishabh Gupta <88454734+officialrishabhgupta@users.noreply.github.com>
2021-12-21 20:38:45 +0530

    typescript

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import './App.css';
+import { Greet } from './components/Greet';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
+import {Person} from './components/Person';
+import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
+
+function App() {
+  const personName = {
+    first:'Bruce',
+    last:'Wayne',
+  }
+
+  const nameList=[
+    {
+      first:'Bruce',
+      last:'Wayne'
+    },
+    {
+      first:'Clark',
+      last:'Kent'
+    },
+    {
+      first:'Princess',
+      last:'Diana'
+    }
+  ]
+  return (
+    <div className="App">
+      <Greet name='Rishabh' messageCount={10} isLoggedIn={true}/>
+      <Person name={personName}/>
+      <PersonList names={nameList}/>
+      <Status status='success'/>
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
+      </Oscar>
+    </div>
+  );
+}
+
+export default App;
